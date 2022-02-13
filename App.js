a431eed5523ecd1cedafa2acec5a4de91211975b
Jardel Sousa <jardeelsousa@hotmail.com>
2022-02-13 12:39:14 -0300

    Criação da estrutura

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { MainLayout } from './src/screens';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}
+        initialRouteName={'Dashboard'}
+      >
+        <Stack.Screen name="Dashboard" component={MainLayout}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/screens/Dashboard/MainLayout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const MainLayout = () => {
+  return(
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>MainLayout</Text>
+    </View>
+  )
+}
+
+export default MainLayout;
